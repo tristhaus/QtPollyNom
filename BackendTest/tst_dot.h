88971fd83cb0de35bf3cac1181c5bb58e206ce03
tristhaus <tristhaus@yahoo.de>
2021-05-29 23:08:25 +0200

    Add license notices to new files

--- BackendTest/tst_dot.h
+++ BackendTest/tst_dot.h
@@ -1,3 +1,21 @@
+/*
+ * This file is part of QtPollyNom.
+ *
+ * QtPollyNom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QtPollyNom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QtPollyNom.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef TST_DOT_H
 #define TST_DOT_H
 
