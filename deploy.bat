f395c830b7fad8c408ca31f320f1d83cdcddaaaa
tristhaus <tristhaus@yahoo.de>
2021-07-07 20:54:40 +0200

    Add license, readme to release directories

--- deploy.bat
+++ deploy.bat
@@ -7,6 +7,8 @@ md .\Deploy-Release
 
 windeployqt --dir .\Deploy-Release ..\build-QtPollyNomSuper-Desktop_Qt_6_0_3_MSVC2019_64bit-Release\QtPollyNom\release\QtPollyNom.exe
 copy ..\build-QtPollyNomSuper-Desktop_Qt_6_0_3_MSVC2019_64bit-Release\QtPollyNom\release\QtPollyNom.exe .\Deploy-Release\
+copy .\LICENSE .\Deploy-Release\
+copy .\README.md .\Deploy-Release\
 
 IF EXIST .\Deploy-Release-PDB (
     rmdir .\Deploy-Release-PDB /s /q
@@ -17,3 +19,5 @@ windeployqt --pdb --dir .\Deploy-Release-PDB ..\build-QtPollyNomSuper-Desktop_Qt
 copy ..\build-QtPollyNomSuper-Desktop_Qt_6_0_3_MSVC2019_64bit-Release\QtPollyNom\release\QtPollyNom.exe .\Deploy-Release-PDB\
 copy ..\build-QtPollyNomSuper-Desktop_Qt_6_0_3_MSVC2019_64bit-Release\QtPollyNom\release\QtPollyNom.map .\Deploy-Release-PDB\
 copy ..\build-QtPollyNomSuper-Desktop_Qt_6_0_3_MSVC2019_64bit-Release\QtPollyNom\release\QtPollyNom.pdb .\Deploy-Release-PDB\
+copy .\LICENSE .\Deploy-Release-PDB\
+copy .\README.md .\Deploy-Release-PDB\
